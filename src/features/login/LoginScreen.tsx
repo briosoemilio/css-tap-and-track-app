4b30734d3f3c80c09efc55125f618b82708b360c
develmo <brioso.emilioluis@gmail.com>
2024-11-03 09:42:47 +0800

    Add login styling

--- src/features/login/LoginScreen.tsx
+++ src/features/login/LoginScreen.tsx
@@ -2,12 +2,28 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import ScreenContainer from "../../components/ScreenContainer";
 import Text from "src/components/Text";
+import TextField from "src/components/TextField";
+import Button from "src/components/Button";
 
 const LoginScreen = () => {
   return (
     <ScreenContainer>
       <Text variant="header2">Login</Text>
-      
+      <TextField label="Email" containerStyle={{ marginTop: 12 }} />
+      <TextField label="Password" containerStyle={{ marginTop: 12 }} />
+      <Button title="Login" style={{ marginTop: 24 }} />
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: 36,
+        }}
+      >
+        <Text variant="body2regular" style={{ marginBottom: 12 }}>
+          Don't have account yet?
+        </Text>
+      </View>
+      <Button title="Click here to sign up" variant="text" />
     </ScreenContainer>
   );
 };
