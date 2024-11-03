ee77be51ae2e03377102f5ea034f651e3fd96ee6
develmo <brioso.emilioluis@gmail.com>
2024-11-03 18:34:56 +0800

    Add account screen UI

--- src/features/account/AccountScreen.tsx
+++ src/features/account/AccountScreen.tsx
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
 import React from "react";
+import { CONSTANTS } from "src/constants/constants";
+import ScreenContainer from "src/components/ScreenContainer";
+import Button from "src/components/Button";
+import { COLORS } from "src/constants/colors";
+import { useAuth } from "src/context/auth/useAuth";
+
+import AccountIconLarge from "@assets/icons/account/account-icon-large.svg";
+import Text from "src/components/Text";
+import { showUnderDevelopment } from "src/helpers/showUnderDevelopment";
 
 const AccountScreen = () => {
+  const { user, onLogout } = useAuth();
+
+  const onPressLogout = () => {
+    Alert.alert("LOGOUT", `Are you sure you want to log out?`, [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Confirm",
+        onPress: () => onLogout(),
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>AccountScreen</Text>
-    </View>
+    <ScreenContainer>
+      <ScrollView
+        style={styles.mainContainer}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.accountInfoContainer}>
+          <AccountIconLarge />
+          <Text variant="body1bold">{user?.name}</Text>
+          {user?.yearSection && (
+            <Text variant="body2regular">{user?.yearSection}</Text>
+          )}
+          <Text variant="body2regular">{user?.idNumber}</Text>
+          <Text variant="body2regular">{user?.email}</Text>
+        </View>
+        <View style={styles.buttonContainers}>
+          <Button
+            title="Change Section"
+            onPress={showUnderDevelopment}
+            style={styles.changeSectionButton}
+          />
+          <Button title="Change Password" onPress={showUnderDevelopment} />
+        </View>
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Log-out"
+          style={{ backgroundColor: COLORS.red }}
+          onPress={onPressLogout}
+        />
+      </View>
+    </ScreenContainer>
   );
 };
 
 export default AccountScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: { paddingHorizontal: CONSTANTS.layout },
+  contentContainer: {
+    flexGrow: 1,
+    alignItems: "center",
+  },
+  buttonContainer: { paddingHorizontal: CONSTANTS.layout, marginBottom: 12 },
+  accountInfoContainer: {
+    marginTop: 100,
+    display: "flex",
+    alignItems: "center",
+  },
+  buttonContainers: {
+    width: "100%",
+    marginTop: 50,
+  },
+  changeSectionButton: { marginBottom: 24 },
+});
