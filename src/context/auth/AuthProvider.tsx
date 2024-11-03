6b6ad689d044bcea919216b0848907c1f6fc3872
develmo <brioso.emilioluis@gmail.com>
2024-11-03 17:58:25 +0800

    Edit bottom tab styling

--- src/context/auth/AuthProvider.tsx
+++ src/context/auth/AuthProvider.tsx
@@ -1,13 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import AsyncStorage from "src/lib/storage/storage";
 import { decodeToken } from "src/lib/jwt/decodeToken";
 import { UserModel } from "src/types/UserModel";
 import { Role } from "src/types/Role";
+import { Session } from "src/lib/session/types";
 
 export const AuthProvider = (props: { children: React.ReactNode }) => {
+  // Hooks
   const [user, setUser] = useState<UserModel>();
   const [isAdmin, setIsAdmin] = useState(false);
+
+  // Functions
+  const decodeTokenAndSetUser = (token: string) => {
+    const decodedToken = decodeToken(token) as UserModel;
+    setUser(decodedToken);
+    if (decodedToken.role === Role.ADMIN) {
+      setIsAdmin(true);
+    }
+  };
+
+  const onAppOpen = async () => {
+    const session = (await AsyncStorage.getItem("session")) as Session;
+    const token = session.token;
+    decodeTokenAndSetUser(token);
+  };
+
+  useEffect(() => {
+    onAppOpen();
+  }, []);
 
   const onLogin = async (res: any) => {
     const { token } = res;
@@ -15,11 +36,7 @@ export const AuthProvider = (props: { children: React.ReactNode }) => {
       token,
     };
     await AsyncStorage.setItem("session", newSession);
-    const decodedToken = decodeToken(token) as UserModel;
-    setUser(decodedToken);
-    if (decodedToken.role === Role.ADMIN) {
-      setIsAdmin(true);
-    }
+    decodeTokenAndSetUser(token);
   };
 
   const onLogout = async () => {
--- src/navigation/AuthNavigator/BottomTabNavStack.tsx
+++ src/navigation/AuthNavigator/BottomTabNavStack.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../../features/home/HomeScreen";
@@ -6,11 +6,53 @@ import AccountScreen from "../../features/account/AccountScreen";
 import ReportScreen from "../../features/report/ReportScreen";
 import { BottomNavParams } from "./BottomNavParams";
 
+import HomeIcon from "@assets/icons/home-icon.svg";
+import ReportIcon from "@assets/icons/report-icon.svg";
+import AccountIcon from "@assets/icons/account-icon.svg";
+import Text from "src/components/Text";
+import { capitalize } from "lodash";
+import { COLORS } from "src/constants/colors";
+
 export const BottomStack = createBottomTabNavigator<BottomNavParams>();
 
 const BottomTabNavStack = () => {
   return (
-    <BottomStack.Navigator initialRouteName="home">
+    <BottomStack.Navigator
+      initialRouteName="home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          const icon = () => {
+            switch (route.name) {
+              case "home":
+                return <HomeIcon />;
+              case "report":
+                return <ReportIcon />;
+              case "account":
+                return <AccountIcon />;
+            }
+          };
+          return icon();
+        },
+        tabBarLabel: () => (
+          <Text variant="body3bold">{capitalize(route.name)}</Text>
+        ),
+        tabBarActiveBackgroundColor: COLORS.darkBlue,
+        tabBarItemStyle: {
+          paddingVertical: 6,
+          marginHorizontal: 8,
+          marginVertical: 6,
+          borderRadius: 8,
+          gap: 6,
+          minHeight: 50,
+        },
+        tabBarAllowFontScaling: false,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          minHeight: 65,
+          backgroundColor: COLORS.black,
+        },
+      })}
+    >
       <BottomStack.Screen name={"home"} component={HomeScreen} />
       <BottomStack.Screen name={"account"} component={AccountScreen} />
       <BottomStack.Screen name={"report"} component={ReportScreen} />
