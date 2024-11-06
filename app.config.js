f34d67dd6ccc338ce215f0450f9b27c8aeb21f95
develmo <brioso.emilioluis@gmail.com>
2024-11-06 13:24:55 +0800

    Additional eas setup

---
+++ app.config.js
@@ -0,0 +1,36 @@
+// app.config.js
+
+export default ({ config }) => {
+  return {
+    ...config,
+    name: "CCS Tap and Track",
+    slug: "ccs-tap-and-track",
+    version: "1.0.0",
+    platforms: ["ios", "android"],
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          android: {
+            usesCleartextTraffic: true,
+          },
+        },
+      ],
+    ],
+    android: {
+      package: "com.ccs.ccstapandtrack",
+      versionCode: 1,
+    },
+    ios: {
+      bundleIdentifier: "com.ccs.ccstapandtrack",
+      buildNumber: "1.0.0",
+    },
+    extra: {
+      eas: {
+        projectId: "a0905f90-bd80-47e7-8e6c-9a18da272ac4",
+      },
+      apiUrl:
+        process.env.EXPO_PUBLIC_API_BASE_URL || "http://47.129.31.141:3000",
+    },
+  };
+};
