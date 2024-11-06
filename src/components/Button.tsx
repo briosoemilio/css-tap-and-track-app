aee6febbed7ec6e96388d75a4aa9f87be9f1afe5
develmo <brioso.emilioluis@gmail.com>
2024-11-06 14:27:44 +0800

    Apply dynamic styling to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,8 +7,11 @@ import {
   TouchableWithoutFeedbackProps,
   ViewStyle,
   ActivityIndicator,
+  StyleProp,
+  View,
+  TextStyle,
 } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
 import { COLORS } from "src/constants/colors";
 import Text from "./Text";
 
@@ -31,25 +34,71 @@ const Button = (props: ButtonComponentProps) => {
     isLoading = false,
     ...rest
   } = props;
+
+  const customStyle = useMemo(() => {
+    let _style: {
+      buttonStyle: StyleProp<ViewStyle>;
+      textStyle: TextStyle;
+    };
+
+    switch (variant) {
+      case "outlined":
+        {
+          if (disabled) {
+            _style = {
+              buttonStyle: styles.buttonOutlinedDisabled,
+              textStyle: styles.textOutlinedDisabled,
+            };
+          } else {
+            _style = {
+              buttonStyle: styles.buttonOutlined,
+              textStyle: styles.textOutlined,
+            };
+          }
+        }
+        break;
+      case "solid":
+        {
+          if (disabled) {
+            _style = {
+              buttonStyle: styles.buttonSolidDisabled,
+              textStyle: styles.textSolidDisabled,
+            };
+          } else {
+            _style = {
+              buttonStyle: styles.buttonSolid,
+              textStyle: styles.textSolid,
+            };
+          }
+        }
+        break;
+      case "text":
+        {
+          if (disabled) {
+            _style = {
+              buttonStyle: styles.buttonTextDisabled,
+              textStyle: styles.textTextDisabled,
+            };
+          } else {
+            _style = {
+              buttonStyle: styles.buttonText,
+              textStyle: styles.textText,
+            };
+          }
+        }
+        break;
+    }
+    return _style;
+  }, [disabled, variant]);
+
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={[
-        styles.button,
-        variant === "solid"
-          ? styles.buttonSolid
-          : variant === "outlined"
-          ? styles.buttonOutlined
-          : styles.buttonText,
-        style,
-      ]}
+      style={[styles.button, customStyle.buttonStyle]}
       disabled={disabled}
       {...rest}
     >
-      <Text
-        variant="body2bold"
-        style={variant === "text" ? styles.textBlue : {}}
-      >
+      <Text variant="body2bold" style={customStyle.textStyle}>
         {title}
         {isLoading && <ActivityIndicator color={COLORS.white} />}
       </Text>
@@ -67,19 +116,51 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  // SOLID
   buttonSolid: {
     backgroundColor: COLORS.blue,
   },
+  buttonSolidDisabled: {
+    backgroundColor: COLORS.gray,
+  },
+  textSolid: {
+    color: COLORS.white,
+  },
+  textSolidDisabled: {
+    color: COLORS.black,
+  },
+
+  // TEXT
   buttonText: {
     backgroundColor: COLORS.transparent,
   },
-  textBlue: {
+  buttonTextDisabled: {
+    backgroundColor: COLORS.transparent,
+  },
+  textText: {
     color: COLORS.blue,
     textDecorationLine: "underline",
   },
+  textTextDisabled: {
+    color: COLORS.gray,
+    textDecorationLine: "underline",
+  },
+
+  // OUTLINED
   buttonOutlined: {
     backgroundColor: COLORS.transparent,
     borderColor: COLORS.blue,
     borderWidth: 5,
+  },
+  buttonOutlinedDisabled: {
+    backgroundColor: COLORS.transparent,
+    borderColor: COLORS.gray,
+    borderWidth: 5,
+  },
+  textOutlined: {
+    color: COLORS.white,
+  },
+  textOutlinedDisabled: {
+    color: COLORS.gray,
   },
 });
