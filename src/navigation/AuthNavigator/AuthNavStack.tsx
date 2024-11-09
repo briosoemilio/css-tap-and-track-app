4069143792d05b34cc6ed1715f44421a1ddaffc2
develmo <brioso.emilioluis@gmail.com>
2024-11-09 10:30:35 +0800

    Added time in screen to auth nav stack

--- src/navigation/AuthNavigator/AuthNavStack.tsx
+++ src/navigation/AuthNavigator/AuthNavStack.tsx
@@ -5,12 +5,13 @@ import {
   NativeStackNavigationProp,
 } from "@react-navigation/native-stack";
 import BottomTabNavStack from "./BottomTabNavStack";
-import HomeScreen from "../../features/home/HomeScreen";
 import TrackNavigator from "src/features/track/TrackNavigator";
+import TimeInScreen from "src/features/timein/screens/TimeInScreen";
 
 export type AuthNavParams = {
   main: undefined;
   track: undefined;
+  "time-in": undefined;
 };
 
 export type AuthNavProps = NativeStackNavigationProp<AuthNavParams>;
@@ -25,6 +26,7 @@ const AuthNavStack = () => {
     >
       <AuthStack.Screen name="main" component={BottomTabNavStack} />
       <AuthStack.Screen name="track" component={TrackNavigator} />
+      <AuthStack.Screen name="time-in" component={TimeInScreen} />
     </AuthStack.Navigator>
   );
 };
