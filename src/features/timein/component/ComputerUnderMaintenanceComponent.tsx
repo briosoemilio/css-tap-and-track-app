da818ace566a55de663f3b105f4837c6247ac8dd
develmo <brioso.emilioluis@gmail.com>
2024-11-16 20:31:25 +0800

    Add under maintenance asset

--- src/features/timein/component/ComputerUnderMaintenanceComponent.tsx
+++ src/features/timein/component/ComputerUnderMaintenanceComponent.tsx
@@ -2,6 +2,9 @@ import { StyleSheet, View } from "react-native";
 import React from "react";
 import Text from "src/components/Text";
 import { ComputerDetails } from "src/services/computer/types";
+import LottieView from "lottie-react-native";
+import UnderMaintenanceAnimation from "@assets/animation/under-maintenance-lottie.json";
+import { COLORS } from "src/constants/colors";
 
 type ComputerUnderMaintenanceComponentProps = {
   computerDetails: ComputerDetails;
@@ -12,8 +15,25 @@ const ComputerUnderMaintenanceComponent = (
 ) => {
   const { computerDetails } = props;
   return (
-    <View>
-      <Text>ComputerUnderMaintenanceComponent</Text>
+    <View
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text variant="header2" textAlign="center" style={{ color: COLORS.red }}>
+        UNDER MAINTENANCE
+      </Text>
+      <LottieView
+        style={{ height: 250, width: 250 }}
+        source={UnderMaintenanceAnimation}
+        autoPlay
+        loop
+      />
+      <Text variant="body2regular" textAlign="center">
+        {`This PC is under maintenance.\nPlease use a different one. Thank you!`}
+      </Text>
     </View>
   );
 };
