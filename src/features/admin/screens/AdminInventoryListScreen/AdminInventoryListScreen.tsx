3935d5a46933dc9e861a56a9f6a5b162632521ea
develmo <brioso.emilioluis@gmail.com>
2024-11-28 10:55:30 +0800

    Add filter icon asset

--- src/features/admin/screens/AdminInventoryListScreen/AdminInventoryListScreen.tsx
+++ src/features/admin/screens/AdminInventoryListScreen/AdminInventoryListScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
 import LottieView from "lottie-react-native";
 
@@ -8,18 +8,21 @@ import Text from "src/components/Text";
 import { CONSTANTS } from "src/constants/constants";
 import Loader from "src/components/Loader";
 import EmptyInventoryLottie from "@assets/animation/empty-inventory-lottie.json";
+import FilterIcon from "@assets/icons/inventory-list/filter-icon.svg";
+import CategoryCard from "./components/CategoryCard";
+import ItemCard from "./components/ItemCard";
+import FilterBottomSheet from "./components/FilterBottomSheet";
 
 // constants
-import { ItemDetails } from "src/services/item/types";
-import { parseCategoryName } from "./utils";
-import { COLORS } from "src/constants/colors";
-import { ItemStatus } from "src/types/ItemStatus";
+import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 
 // hooks
 import { useGetCategoryList } from "./hooks/useGetCategoryList";
 import { useGetItemList } from "./hooks/useGetItemList";
 
 const AdminInventoryListScreen = () => {
+  const bottomSheetRef = useRef<BottomSheetMethods>(null);
+
   const { categoryList, selectedCategory, onPressCategory, categoryListRef } =
     useGetCategoryList();
 
@@ -42,7 +45,18 @@ const AdminInventoryListScreen = () => {
   return (
     <ScreenContainer>
       <View style={styles.mainContainer}>
-        <Text variant="body1bold">Inventory List</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text variant="body1bold">Inventory List</Text>
+          <TouchableOpacity onPress={() => bottomSheetRef?.current?.expand()}>
+            <FilterIcon />
+          </TouchableOpacity>
+        </View>
         <View style={{ marginTop: 12 }}>
           <FlatList
             ref={categoryListRef}
@@ -77,7 +91,7 @@ const AdminInventoryListScreen = () => {
                 <View>
                   <LottieView
                     source={EmptyInventoryLottie}
-                    style={{ height: 300, width: 300 }}
+                    style={{ height: 300 }}
                     autoPlay
                     loop
                   />
@@ -90,52 +104,9 @@ const AdminInventoryListScreen = () => {
           />
         </View>
       </View>
+
+      <FilterBottomSheet bottomSheetRef={bottomSheetRef} />
     </ScreenContainer>
-  );
-};
-
-const CategoryCard = (props: {
-  name: string;
-  isSelected: boolean;
-  onPress: () => void;
-}) => {
-  const { name, isSelected, onPress } = props;
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[styles.categoryCard, isSelected && styles.selectedCategory]}
-    >
-      <Text variant="body2regular">{parseCategoryName(name)}</Text>
-    </TouchableOpacity>
-  );
-};
-
-const ItemCard = (props: { itemDetails: ItemDetails }) => {
-  const { itemDetails } = props;
-  const { name, locationName, categoryName, status } = itemDetails;
-  return (
-    <TouchableOpacity style={styles.itemCard}>
-      <View>
-        <Text variant="body2bold">{name}</Text>
-        <Text variant="body3regular">{locationName}</Text>
-      </View>
-      <View>
-        <Text variant="body3regular" textAlign="right">
-          {categoryName}
-        </Text>
-        <Text
-          variant="body3regular"
-          textAlign="right"
-          style={
-            status === ItemStatus.UNDER_MAINTENANCE
-              ? { color: COLORS.red }
-              : { color: COLORS.green }
-          }
-        >
-          {status}
-        </Text>
-      </View>
-    </TouchableOpacity>
   );
 };
 
@@ -143,24 +114,4 @@ export default AdminInventoryListScreen;
 
 const styles = StyleSheet.create({
   mainContainer: { flex: 1, paddingHorizontal: CONSTANTS.layout },
-  categoryCard: {
-    paddingHorizontal: 18,
-    borderColor: COLORS.blue,
-    borderWidth: 3,
-    marginRight: 12,
-    borderRadius: 12,
-  },
-  selectedCategory: { backgroundColor: COLORS.darkBlue },
-  itemCard: {
-    paddingVertical: 4,
-    paddingHorizontal: 12,
-    borderColor: COLORS.blue,
-    borderWidth: 3,
-    marginBottom: 12,
-    borderRadius: 12,
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
 });
