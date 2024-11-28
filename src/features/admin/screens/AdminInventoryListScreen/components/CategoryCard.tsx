788066d7c9092531fed200a6e40f9ed08ad9027a
develmo <brioso.emilioluis@gmail.com>
2024-11-28 10:55:44 +0800

    Create separate categorycard and itemcard component

---
+++ src/features/admin/screens/AdminInventoryListScreen/components/CategoryCard.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { COLORS } from "src/constants/colors";
+import { parseCategoryName } from "../utils";
+import Text from "src/components/Text";
+
+const CategoryCard = (props: {
+  name: string;
+  isSelected: boolean;
+  onPress: () => void;
+}) => {
+  const { name, isSelected, onPress } = props;
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.categoryCard, isSelected && styles.selectedCategory]}
+    >
+      <Text variant="body2regular">{parseCategoryName(name)}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CategoryCard;
+
+const styles = StyleSheet.create({
+  categoryCard: {
+    paddingHorizontal: 18,
+    borderColor: COLORS.blue,
+    borderWidth: 3,
+    marginRight: 12,
+    borderRadius: 12,
+  },
+  selectedCategory: { backgroundColor: COLORS.darkBlue },
+});
---
+++ src/features/admin/screens/AdminInventoryListScreen/components/ItemCard.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Text from "src/components/Text";
+import { COLORS } from "src/constants/colors";
+import { ItemStatus } from "src/types/ItemStatus";
+import { ItemDetails } from "src/services/item/types";
+
+const ItemCard = (props: { itemDetails: ItemDetails }) => {
+  const { itemDetails } = props;
+  const { name, locationName, categoryName, status } = itemDetails;
+  return (
+    <TouchableOpacity style={styles.itemCard}>
+      <View>
+        <Text variant="body2bold">{name}</Text>
+        <Text variant="body3regular">{locationName}</Text>
+      </View>
+      <View>
+        <Text variant="body3regular" textAlign="right">
+          {categoryName}
+        </Text>
+        <Text
+          variant="body3regular"
+          textAlign="right"
+          style={
+            status === ItemStatus.UNDER_MAINTENANCE
+              ? { color: COLORS.red }
+              : { color: COLORS.green }
+          }
+        >
+          {status}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ItemCard;
+
+const styles = StyleSheet.create({
+  itemCard: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    borderColor: COLORS.blue,
+    borderWidth: 3,
+    marginBottom: 12,
+    borderRadius: 12,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
