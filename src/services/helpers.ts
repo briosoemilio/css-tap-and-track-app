54005e2ee0c155f39256d1621f5a8614ccbfccc0
develmo <brioso.emilioluis@gmail.com>
2024-11-30 15:31:10 +0800

    Update getErrorMessage in helpers

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -1,7 +1,7 @@
 export const getErrorMessage = (err: any) => {
   if (err instanceof Error) {
     const errMessage = JSON.parse(err.message);
-    return errMessage?.message;
+    return errMessage?.message || "An unknown error occurred.";
   } else {
     return JSON.stringify(err);
   }
