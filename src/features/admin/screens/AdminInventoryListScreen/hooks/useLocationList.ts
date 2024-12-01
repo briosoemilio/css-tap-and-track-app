21d42f6e808fb3e46362e6321796b1d48fb16d8c
develmo <brioso.emilioluis@gmail.com>
2024-12-01 10:18:38 +0800

    modify useLocationList to optionally insert All

--- src/features/admin/screens/AdminInventoryListScreen/hooks/useLocationList.ts
+++ src/features/admin/screens/AdminInventoryListScreen/hooks/useLocationList.ts
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getAllLocations } from "src/services/location/getAllLocations";
 import { LocationDetails } from "src/services/location/types";
 
-export const useLocationList = () => {
+export const useLocationList = (insertAll = true) => {
   const [locationList, setLocationList] = useState<LocationDetails[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -10,8 +10,12 @@ export const useLocationList = () => {
     setIsLoading(true);
     try {
       const { data } = await getAllLocations();
-      const all = { name: "ALL" } as LocationDetails;
-      setLocationList([all, ...data]);
+      if (insertAll) {
+        const all = { name: "ALL" } as LocationDetails;
+        setLocationList([all, ...data]);
+      } else {
+        setLocationList([...data]);
+      }
     } catch (err) {
       console.log("Error loadLocations invoke inside hooks -> ", err);
     } finally {
